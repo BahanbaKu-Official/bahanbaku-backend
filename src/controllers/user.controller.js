bf32f60d3868cf9ed3ef0498f8b59e36e7db45b9
rifqihz <rifqi977@gmail.com>
2022-11-18 22:25:51 +0700

    add upload profile image route and controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const db = require('../models');
+const cloudStorage = require('../config/cloudstorage.config');
 const User = db.user;
 
 const getUserById = async (req, res, next) => {
@@ -17,6 +18,52 @@ const getUserById = async (req, res, next) => {
   }
 }
 
+const uploadPicture = async (req, res, next) => {    
+  const { userId } = req.user;
+  const bucketName = process.env.CLOUD_STORAGE_BUCKET;
+
+  try {
+    const user = await User.findByPk(userId);
+    
+    if (!user) return next('404,user not listed');
+    
+    const bucket = cloudStorage.bucket(bucketName);    
+    const { originalname, buffer } = req.file;
+    const ext = originalname.split('.')[1];    
+    const blob = bucket.file(`user/${userId}.${ext}`);
+    const blobStream = blob.createWriteStream({
+      resumeable: false,
+    })
+
+    blobStream
+      .on('finish', async () => {
+        const profileImage = `https://storage.googleapis.com/${bucketName}/${blob.name}`;
+        
+        const updateUser = await User.update({
+          profileImage,
+        }, {
+          where: {
+            userId,
+          }
+        })
+
+        if (!updateUser) return '500,update profile failed';
+
+        return res.status(200).json({
+          status: true,
+          message: 'success update user profile picture',
+          results: {
+            userId
+          },
+        })
+      })
+      .end(buffer);
+  } catch (error) {
+    next(error);
+  }
+}
+
 module.exports = {
   getUserById,
+  uploadPicture
 }
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,7 +1,9 @@
 const user = require('../controllers/user.controller');
 const router = require('express').Router();
 const jwtMiddleware = require('../middlewares/jwtAuth')
+const multer = require('../config/multer.config');
 
 router.get('/profile', jwtMiddleware, user.getUserById);
+router.post('/update/picture', jwtMiddleware, multer.single('profile-file'), user.uploadPicture);
 
 module.exports = router;
